5c1269d53979e212e3abafc793e4c05dccba0feb
AaroN <aaro.niinisto@aalto.fi>
2022-12-29 14:27:46 +0200

    tehty osan 3 tehtävät

--- index.js
+++ index.js
@@ -1,104 +1,132 @@
+require('dotenv').config()
 const { response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('data', (req, res) => {
-    const data = req.body
-    if (!data.name){
-        return ''
-    }
-    return `{"name":${data.name},"number":${data.number}}`
+  const data = req.body
+  if (!data.name){
+    return ''}
+  return `{"name":${data.name},"number":${data.number}}`
 })
+
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 app.use(cors()) 
 app.use(express.static('build'))
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Mary Poppendick",
-        number: "39-23-6423122"
-    }
-]
+//let persons = [
+//{
+//      id: 1,
+//    name: "Arto Hellas",
+//  number: "040-123456"
+//},
+// {
+//  id: 2,
+//  name: "Ada Lovelace",
+//   number: "39-44-5323523"
+// },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//    {
+//        id: 4,
+//        name: "Mary Poppendick",
+//        number: "39-23-6423122"
+//     }
+// ]
 
 //3.1
-app.get('/persons', (req, res) => {
+app.get('/api/persons', (req, res) => {
+  //res.json(persons)
+  Person.find({}).then(persons => {
     res.json(persons)
   })
+})
 
   //3.2
 app.get('/info', (req, res) => {
+  Person.find({}).then(persons => {
     const amount = persons.length
     const date = new Date()
     res.send(`<div><div>The phonebook has info for ${amount}</div><div>${date}</div></div>`)
+  })
 })
 
 //3.3
-app.get('/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person){
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person =>{
+      if (person){
         res.json(person)
-    }
-    else {
+      }
+      else{
         res.status(404).end()
-    }
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/persons', (req, res) => {
-    const person = req.body
-    if (!person.name){
-        return res.status(400).json({ 
-            error: 'name missing' 
-          })
-    }
-    else if(!person.number){
-        return res.status(400).json({
-            error: "number missing"
-        })
-    }
-    else if (persons.find(p => person.name === p.name)){
-        return res.status(400).json({
-            error: "name must be unique"
-        })
-    }
-    else{
-    const id = Math.floor(Math.random()*10000)
-    person.id = id
-    persons = persons.concat(person)
-    
-    res.json(person)
-    }  
-    
-    
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
+  //ei tarkastusta nimen yksikäsitteisyydelle
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+})
+  person.save().then((savedPerson) => res.json(savedPerson)).catch(error => next(error))   
 })
 
-const PORT = 8080 //process.env.PORT //|| 3001  - process.env.PORT ei jostain syystä toiminut. Palauttaa undefined.
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+}
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true, runValidators: true, context: 'query'})
+    .then(updatedPerson => {
+      if (updatedPerson){
+        res.json(updatedPerson)
+      }
+      else{       
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === "ValidationError"){
+    return res.status(400).json({error: error.message})
+  }
+  
+  next(error)
+}
+  
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 8080 //process.env.PORT //|| 3001 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- mongo.js
+++ mongo.js
@@ -1,44 +1,41 @@
 const mongoose = require('mongoose')
-
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
-  }
+  console.log('give password as argument')
+  process.exit(1)
+}
 else if (process.argv.length === 4){
-    process.exit(1)
+  process.exit(1)
 }
 else {
-const password = process.argv[2]
+  const password = process.argv[2]
 
-const url = `mongodb+srv://AaroNiFullstack:${password}@cluster0.rj4hpfi.mongodb.net/?retryWrites=true&w=majority`
+  const url = `mongodb+srv://AaroNiFullstack:${password}@cluster0.rj4hpfi.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(url)
+  mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
+  const personSchema = new mongoose.Schema({
     name: String,
     number: String
   })
 
 
-const Person = mongoose.model('Person', personSchema)
+  const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length === 3){
-    console.log("Phonebook:")
-    Person
-  .find({})
-  .then(persons => {
-    persons.forEach(person => {console.log(`${person.name} ${person.number}`)})
-    mongoose.connection.close()
+  if (process.argv.length === 3){
+    console.log('Phonebook:')
+    Person.find({}).then(persons => {
+      persons.forEach(person => {console.log(`${person.name} ${person.number}`)})
+      mongoose.connection.close()
 })
 }
-else{
-const person = new Person({
-    name: process.argv[3],
-    number: process.argv[4]
-  })
+  else{
+    const person = new Person({
+      name: process.argv[3],
+      number: process.argv[4]
+    })
   
-  person.save().then(result => {
-    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
-    mongoose.connection.close()
-  })
-}}
+    person.save().then(result => {
+      console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+      mongoose.connection.close()
+    })
+  }}
